6c4e2d4c33d01e47692fe262f87acbb6ff0a035c
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-03-09 14:14:03 +0200

    задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+function createGalleryCardItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </div>`;
+    }).join('');
+    
+}
+
+ var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
