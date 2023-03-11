af20bd7e86e7d1038f6ae1b931bef31fb87ef179
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-03-11 14:26:32 +0200

    hw 8js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+// import player from '@vimeo/player';
+
+// const player = new Vimeo.Player(iframe);
+
+   
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime() {
+  player.getCurrentTime().then(function(seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+});
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,70 @@
+import throttle from 'lodash.throttle';
 
+
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('.feedback-form input');
+const messageEl = document.querySelector('.feedback-form textarea');
+formEl.addEventListener('input', throttle(handleInput,500));
+formEl.addEventListener('submit', handleFormSubmit);
+
+const STORAGE_KEY = "feedback-form-state";
+
+populateForm();
+
+const feedbackForm = {};
+function handleInput(event) {
+    feedbackForm[event.target.name] = event.target.value;
+    const feedbackFormJSON = JSON.stringify(feedbackForm);
+    localStorage.setItem(STORAGE_KEY, feedbackFormJSON);
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(feedbackForm);
+}
+
+function populateForm() {
+    const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (formData) {
+        emailEl.value = formData.email;
+        messageEl.value = formData.message;
+    }
+}
+
+// import throttle from 'lodash.throttle';
+
+// const formEl = document.querySelector('.feedback-form');
+// const emailEl = formEl.querySelector('input[name="email"]');
+// const messageEl = formEl.querySelector('textarea[name="message"]');
+// const STORAGE_KEY = "feedback-form-state";
+
+// const feedbackForm = {};
+
+// function handleInput(event) {
+//   feedbackForm[event.target.name] = event.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
+// }
+
+// function handleFormSubmit(event) {
+//   event.preventDefault();
+//   event.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+//   console.log(feedbackForm);
+// }
+
+// function populateForm() {
+//   const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   if (formData) {
+//     emailEl.value = formData.email;
+//     messageEl.value = formData.message;
+//     feedbackForm.email = formData.email;
+//     feedbackForm.message = formData.message;
+//   }
+// }
+
+// formEl.addEventListener('input', throttle(handleInput, 500));
+// formEl.addEventListener('submit', handleFormSubmit);
+
+// populateForm();
